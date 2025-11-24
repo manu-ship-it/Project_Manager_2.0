7cd22d30de12650bdea8ce4c6529fb906400814f
Manu.MJOIN <manu@morejoinery.com>
2025-11-24 19:52:15 +1100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  /* config options here */
   eslint: {
     // Don't fail build on ESLint errors - this is critical for Railway deployment
     ignoreDuringBuilds: true,
